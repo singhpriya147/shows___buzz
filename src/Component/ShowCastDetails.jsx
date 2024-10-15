8880ec1dad7b598fda88e32675665e4e189c13d7
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2024-10-16 00:14:00 +0530

    added style to cast card

--- src/Component/ShowCastDetails.jsx
+++ src/Component/ShowCastDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import   '../styles/ShowCastDetails.css'
+import { Card } from '@mui/material';
 const ShowCastDetails = () => {
   const { showId } = useParams();
   console.log("from param");
@@ -22,26 +23,29 @@ const ShowCastDetails = () => {
     <div className='container'>
       <h1>Casts of your favorite Shows</h1>
       <div className='detail-container'>
-        {showCast && showCast.map((character) => (
-          <div key={character.person.id} className='actor-details'>
-            {/* Using a unique identifier as the key */}
-            <h2>{character.character.name}</h2>
-            {character.character.image && ( // Check if character image exists
-              <img
-                src={character.character.image.medium}
-                alt={character.character.name}
-              />
-            )}
-            <p>Name: {character.person.name}</p>
-            {character.person.birthday && ( // Check if birthday exists
-              <p>Birthday: {character.person.birthday}</p>
-            )}
-            {character.person.gender && ( // Check if gender exists
-              <p>Gender: {character.person.gender}</p>
-            )}
-            {/* You can add conditional rendering for other character details */}
-          </div>
-        ))}
+        {showCast &&
+          showCast.map((character) => (
+            <Card style={{padding:'2rem',textAlign:'center'}}>
+              <div key={character.person.id} >
+                {/* Using a unique identifier as the key */}
+                <h2>{character.character.name}</h2>
+                {character.character.image && ( // Check if character image exists
+                  <img
+                    src={character.character.image.medium}
+                    alt={character.character.name}
+                  />
+                )}
+                <p>Name: {character.person.name}</p>
+                {character.person.birthday && ( // Check if birthday exists
+                  <p>Birthday: {character.person.birthday}</p>
+                )}
+                {character.person.gender && ( // Check if gender exists
+                  <p>Gender: {character.person.gender}</p>
+                )}
+                {/* You can add conditional rendering for other character details */}
+              </div>
+            </Card>
+          ))}
       </div>
     </div>
   );
